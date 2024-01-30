5e02080a839285ef1d0596dee4db2dc0e6970b76
dmerk2 <dan.merkin@gmail.com>
2024-01-30 12:35:48 -0500

    Initial Commit

---
+++ client/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: import.meta.env || 3000,
+    open: true
+  }
+})
