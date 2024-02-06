9ae9dede1f43792913cc975cd642ff0ef99ca9f9
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2024-02-06 12:04:35 -0500

    Added root colors and font. Display header component

--- client/src/layouts/Home.jsx
+++ client/src/layouts/Home.jsx
@@ -1,11 +1,10 @@
+import Header from "../../components/header/Header"
+
 function Home() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h1 className="text-center">Home</h1>
-        </div>
-      </div>    
+    <div>
+      <Header />
+      <h1>Home</h1>
     </div>
   )
 }
