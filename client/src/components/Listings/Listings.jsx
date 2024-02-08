2b9a5a7ab6cd34c94cc8fdf4295a7af870b3e719
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2024-02-08 16:57:46 -0500

    Attempting to fix errors with Listings.jsx

--- client/src/components/Listings/Listings.jsx
+++ client/src/components/Listings/Listings.jsx
@@ -1,71 +1,78 @@
 import { useQuery } from "@apollo/client";
-import { QUERY_USERS, getUserRole } from "../../utils/queries";
+import { QUERY_USERS } from "../../utils/queries";
+import auth from "../../utils/auth";
 
 function Listings() {
-  const { data: roleData } = useQuery(getUserRole);
-  const role = roleData ? roleData.getUserRole : null;
+
+    const loggedIn = auth.loggedIn();
+  let role = null;
+
+  if (loggedIn) {
+    role = "employer";
+  }
 
   const { loading, data } = useQuery(QUERY_USERS, {
     variables: {
-      role: role === 'employee' ? 'employer' : 'employee',
+      role: role,
     },
   });
-
 
   if (loading) return <div>Loading...</div>;
 
   const employers = data?.users || [];
-  console.log(role);
 
   return (
     <section>
-      <div className="container mx-auto px-6 py-20">
-        <div className="flex justify-center">
-          {employers.map((employer) => (
-            <div
-              key={employer._id}
-              className="max-w-sm rounded overflow-hidden shadow-lg bg-white border border-gray-200 mx-4 mb-8"
-            >
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2 ">
-                  {employer.username}
-                </div>
-                <a href={`mailto:${employer.email}`} style={{ color: "blue" }}>
-                  {employer.email}
-                </a>
-                <p className="text-gray-700 mb-4">{employer.bio}</p>
-                <div className="font-bold text-lg">{employer.location}</div>
-              </div>
-              <div className="px-6 pt-4 pb-2">
-                {employer.skills.map((skill, index) => (
-                  <span
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                    key={index}
+        <div className="container mx-auto px-6 py-20">
+          <div className="flex justify-center">
+            {employers.map((employer) => (
+              <div
+                key={employer._id}
+                className="max-w-sm rounded overflow-hidden shadow-lg bg-white border border-gray-200 mx-4 mb-8"
+              >
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2 ">
+                    {employer.username}
+                  </div>
+                  <a
+                    href={`mailto:${employer.email}`}
+                    style={{ color: "blue" }}
                   >
-                    #{skill}
-                  </span>
-                ))}
-              </div>
-              <div className="px-6 pb-2">
-                <div className="font-bold text-lg">Other Listings</div>
-                <div>
-                  {employer.listedJobs.map((job, index) => (
-                    <a
+                    {employer.email}
+                  </a>
+                  <p className="text-gray-700 mb-4">{employer.bio}</p>
+                  <div className="font-bold text-lg">{employer.location}</div>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                  {employer.skills.map((skill, index) => (
+                    <span
                       className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
                       key={index}
-                      href={`/employees/${employer._id}/${job._id}`}
                     >
-                      {job.title}
-                    </a>
+                      #{skill}
+                    </span>
                   ))}
                 </div>
+                <div className="px-6 pb-2">
+                  <div className="font-bold text-lg">Other Listings</div>
+                  <div>
+                    {employer.listedJobs.map((job, index) => (
+                      <a
+                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                        key={index}
+                        href={`/employees/${employer._id}/${job._id}`}
+                      >
+                        {job.title}
+                      </a>
+                    ))}
+                  </div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+  )
 }
 
-export default Listings;
+export default Listings
