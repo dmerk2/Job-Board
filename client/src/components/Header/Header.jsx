85eb0134b9bd160971844334aed8167843a335d2
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2024-02-08 12:54:58 -0500

    Added to Header, and fixed color issue

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,35 +2,71 @@ import "./Header.css";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/Hop_On_Board_Logo.png";
+import { useState } from "react";
 
 const Header = () => {
-  return (
-    <header className="ui pointing menu header">
-      <img src= { Logo } alt="Hop On Board Logo" className="item text-3xl" width="75"></img>
-      <Link to="/" className="item text-3xl ">
-        Hop On Board
-      </Link>
-      <nav className="right menu flex text-camelot text-xl">
-        {Auth.loggedIn() ? (
-          <>
-           <Link to="/employers/home" className="item pr-2 hover:border-b-2 border-camelot"> 
-            Applicants
-            </Link>
-            <p> | </p>
-            <Link to="/employees/home" className="item pl-2 pr-2 hover:border-b-2 border-camelot"> 
-            Jobs
-            </Link>
-            <p> | </p>
-            <Link onClick={() => Auth.logout()} className="item pl-2 hover:border-b-2 border-camelot">
-              Log Out
-            </Link>
+  const [isOpen, setIsOpen] = useState(false);
 
-          </>
-        ) : (
-          <Link to="/login" className="item">
-            Log In
-          </Link>
-        )}
+  return (
+    <header className="header grid grid-cols-3 items-center">
+      <div className="flex justify-start md:justify-start md:order-1">
+        <img
+          src={Logo}
+          alt="Hop On Board Logo"
+          className="item text-3xl"
+          width="75"
+        ></img>
+      </div>
+      <div className="flex justify-center order-2 md:order-2">
+        <Link to="/" className="item text-3xl">
+          Hop On Board
+        </Link>
+      </div>
+      <nav className="right menu flex justify-end order-3 md:order-3 text-camelot text-xl">
+        <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
+          Menu
+        </button>
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } md:flex md:items-center md:space-x-4`}
+        >
+          {Auth.loggedIn() ? (
+            <>
+              <Link
+                to="/employers/home"
+                className="item pr-2 hover:border-b-2 border-camelot"
+              >
+                Applicants
+              </Link>
+              <p className="hidden md:block"> | </p>
+              <Link
+                to="/employees/home"
+                className="item pl-2 pr-2 hover:border-b-2 border-camelot"
+              >
+                Jobs
+              </Link>
+              <p className="hidden md:block"> | </p>
+              <Link
+                to="/:userId/profile"
+                className="item pl-2 pr-2 hover:border-b-2 border-camelot"
+              >
+                Profile
+              </Link>
+              <p className="hidden md:block"> | </p>
+              <Link
+                onClick={() => Auth.logout()}
+                className="item pl-2 hover:border-b-2 border-camelot"
+              >
+                Log Out
+              </Link>
+            </>
+          ) : (
+            <Link to="/login" className="item">
+              Log In
+            </Link>
+          )}
+        </div>
       </nav>
     </header>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: [
     './src/**/*.html',
@@ -9,13 +11,15 @@ export default {
     './public/index.html',
   ],
   theme: {
-    extend: {},
-    colors: {
-      'athens_gray': '#e1e3e8',
-      'blue_marguerite': '#8470c0',
-      'meteorite': '#3a1d7b',
-      'camelot': '#912b63',
-    }
+    extend: {
+      colors: {
+        'athens_gray': '#e1e3e8',
+        'blue_marguerite': '#8470c0',
+        'meteorite': '#3a1d7b',
+        'camelot': '#912b63',
+      }
+    },
+    
   },
   plugins: [],
 }
