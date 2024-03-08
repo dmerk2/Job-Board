aed1325f87b0a478d275b47ac45554fe0c224fa0
dmerk2 <dan.merkin@gmail.com>
2024-03-08 13:49:10 -0500

    Configuring s3 bucket in server

--- client/src/layouts/UserBoard.jsx
+++ client/src/layouts/UserBoard.jsx
@@ -5,9 +5,9 @@ import Loading from "../components/Loading/Loading.jsx";
 import Employee_Card from "../components/Job_Cards/Employee_Card.jsx";
 
 function UserBoard() {
-  const role = getUserRole()
+  const role = getUserRole();
 
-  const queryRole = role === 'employer' ? 'employee' : role;
+  const queryRole = role === "employer" ? "employee" : role;
 
   const { loading, data } = useQuery(QUERY_USERS, {
     variables: {
@@ -15,14 +15,14 @@ function UserBoard() {
     },
   });
 
-  if (loading) return <Loading/>;
+  if (loading) return <Loading />;
 
   const employees = data?.users || [];
 
   return (
     <section className="justify-center py-12 px-4 sm:px-6 lg:px-8">
       <h1 className="text-3xl text-center font-bold mb-4">Employees</h1>
-     < Employee_Card employees = {employees} />
+      <Employee_Card employees={employees} />
     </section>
   );
 }
